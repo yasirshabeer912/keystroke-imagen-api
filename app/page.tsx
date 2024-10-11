75de2ccbc65e53185763bed0ac82712eaf57a3c0
yasirshabeer912 <imyasirshabeer@gmail.com>
2024-10-11 23:47:56 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,160 @@
+"use client";
+import Spinner from "@/components/spinner";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@uidotdev/usehooks";
+import { useQuery } from "@tanstack/react-query";
+
+type ImageResponse = {
+  b64_json: string;
+  timings: { inference: number };
+};
+
+export default function Home() {
+  const [userAPIKey, setUserAPIKey] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [iterativeMode, setIterativeMode] = useState(false);
+  const debouncedPrompt = useDebounce(prompt, 300);
+  const [generations, setGenerations] = useState<
+    {
+      prompt: string;
+      image: ImageResponse;
+    }[]
+  >([]);
+  let [activeIndex, setActiveIndex] = useState<number>();
+
+  const { data: image, isFetching } = useQuery({
+    placeholderData: (previousData) => previousData,
+    queryKey: [debouncedPrompt],
+    queryFn: async () => {
+      let res = await fetch("/api/generateImage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt, userAPIKey, iterativeMode }),
+      });
+
+      if (!res.ok) {
+        throw new Error(await res.text());
+      }
+
+      return (await res.json()) as ImageResponse;
+    },
+    enabled: !!debouncedPrompt.trim(),
+    staleTime: Infinity,
+    retry: false,
+  });
+
+  let isDebouncing = prompt !== debouncedPrompt;
+
+  useEffect(() => {
+    if (image && !generations.map((g) => g.image).includes(image)) {
+      setGenerations((images) => [...images, { prompt, image }]);
+      setActiveIndex(generations.length);
+    }
+  }, [generations, image, prompt]);
+
+  let activeImage =
+    activeIndex !== undefined ? generations[activeIndex].image : undefined;
+  return (
+    <div className="flex h-full flex-col px-5">
+      <header className="flex justify-center pt-20 md:justify-end md:pt-3">
+        <div>
+          <h1 className="font-bold text-4xl bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent">
+            KeyStrokeImagen
+          </h1>
+        </div>
+        <div>
+         
+         
+        </div>
+      </header>
+
+      <div className="flex justify-center">
+        <form className="mt-10 w-full max-w-lg">
+          <fieldset>
+            <div className="relative">
+              <Textarea
+                rows={4}
+                spellCheck={false}
+                placeholder="Describe your image..."
+                required
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                className="w-full resize-none border-gray-300 border-opacity-50 bg-gray-400 px-4 text-base placeholder-gray-300"
+              />
+              <div>
+                <Spinner className="size-4" />
+              </div>
+            </div>
+
+            <div className="mt-3 text-sm md:text-right">
+              <label
+                title="Use earlier images as references"
+                className="inline-flex items-center gap-2"
+              >
+                Consistency mode
+                <Switch
+                  checked={iterativeMode}
+                  onCheckedChange={setIterativeMode}
+                />
+              </label>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+      <div className="flex w-full grow flex-col items-center justify-center pb-8 pt-4 text-center">
+        {!activeImage || !prompt ? (
+          <div className="max-w-xl md:max-w-4xl lg:max-w-3xl">
+            <p className="text-xl font-semibold text-gray-200 md:text-3xl lg:text-4xl">
+              Generate images in real time
+            </p>
+            <p className="mt-4 text-balance text-sm text-gray-300 md:text-base lg:text-lg">
+              Enter a prompt and generate images in milliseconds as you keep on
+              typing.
+            </p>
+          </div>
+        ) : (
+          <div className="mt-4 flex w-full max-w-4xl flex-col justify-center">
+            <div>
+              <Image
+                width={1024}
+                height={768}
+                src={`data:image/png;base64,${activeImage.b64_json}`}
+                alt=""
+                className={`${
+                  isFetching ? "animate-pulse" : ""
+                } max-w-full rounded-lg object-cover shadow-sm shadow-black`}
+              />
+            </div>
+
+            <div className="mt-4 flex gap-4 overflow-x-scroll pb-4">
+              {generations.map((generatedImage, i) => (
+                <button
+                  key={i}
+                  className="w-32 shrink-0 opacity-50 hover:opacity-100"
+                  onClick={() => setActiveIndex(i)}
+                >
+                  <Image
+                    width={1024}
+                    height={768}
+                    src={`data:image/png;base64,${generatedImage.image.b64_json}`}
+                    alt=""
+                    className={`${
+                      isFetching ? "animate-pulse" : ""
+                    } max-w-full rounded-lg object-cover shadow-sm shadow-black`}
+                  />
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
